fc6edc59428c00f4771071c6a934d990d1c7e572
{Jayendra <{jay.bharti2804@gmail.com}>
2025-05-27 12:29:31 +0530

    fixed

--- components/SignInWithGoogle.tsx
+++ components/SignInWithGoogle.tsx
@@ -9,7 +9,7 @@ export default function SignInWithGoogle() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `/auth/callback`,
       },
     });
   };
